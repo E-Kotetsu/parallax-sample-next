ff23efe1e6419d97da5df0a5db6f8f4d68aa312a
E-Kotetsu <s.kodera28@gmail.com>
2024-08-12 17:52:18 +0900

    sass導入

---
+++ animation-app/app/sample/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+const SamplePage = () => {
+    return (
+        <>
+            <Header />
+            SamplePage
+            <Footer />
+        </>
+    )
+}
+export default SamplePage;
